909a38cfaba5c9bb85f8f69349b77f3f84f3eeb2
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2020-05-04 09:52:13 -0400

    initialized basic files

---
+++ astir/astir.py
@@ -0,0 +1,189 @@
+# astir: Automated aSsignmenT sIngle-cell pRoteomics
+
+# VSCode tips for python:
+# ctrl+` to open terminal
+# cmd+P to go to file
+
+
+import torch
+from torch.autograd import Variable
+from torch.distributions import Normal
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+import pandas as pd
+import numpy as np
+import yaml
+
+from sklearn.preprocessing import StandardScaler
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Run astir')
+
+## argparse treats the options we give it as strings by default
+
+parser.add_argument("expr_csv", help="CSV expression matrix with cells as rows and proteins as columns. First column is cell ID")
+parser.add_argument("marker_yaml", help="YAML file of cell markers")
+parser.add_argument("output_csv", help="Output CSV of cell assignment probabilities")
+parser.add_argument("--epochs", 
+                    help="Number of training epochs",
+                    type=int,
+                    default=100)
+parser.add_argument("--learning_rate",
+                    help="Learning rate",
+                    type=float,
+                    default=1e-2)
+parser.add_argument("--batch_size",
+                    help="Batch size",
+                    type=int,
+                    default=1024)
+parser.add_argument("--random_seed",
+                    help="Random seed",
+                    type=int,
+                    default=1234)
+
+args = parser.parse_args()
+
+torch.manual_seed(args.random_seed)
+
+# print((args.expr_csv, args.marker_yaml, args.output_csv))
+
+## Load the gene expression data
+df_gex = pd.read_csv(args.expr_csv, index_col = 0)
+
+core_names = list(df_gex.index)
+gene_names = list(df_gex.columns)
+
+## Load the marker info data
+with open(args.marker_yaml, 'r') as stream:
+    markers_states = yaml.safe_load(stream)
+    
+markers = markers_states['cell_types']
+
+cell_types = list(markers.keys())
+genes = [l for s in markers.values() for l in s]
+genes = list(set(genes)) # Make unique
+
+## Infer dimensions
+N = df_gex.shape[0]
+G = len(genes)
+C = len(cell_types)
+
+## Construct marker matrix
+marker_mat = np.zeros(shape = (G,C+1))
+
+for g in range(len(genes)):
+    for ct in range(len(cell_types)):
+        gene = genes[g]
+        cell_type = cell_types[ct]
+        
+        if gene in markers[cell_type]:
+            marker_mat[g,ct] = 1
+
+Y_np = df_gex[genes].to_numpy()
+
+## Dataset class: for loading IMC datasets
+class IMCDataSet(Dataset):
+    
+    def __init__(self, Y_np):
+             
+        self.Y = torch.from_numpy(Y_np)
+        X = StandardScaler().fit_transform(Y_np)
+        self.X = torch.from_numpy(X)
+    
+    def __len__(self):
+        return self.Y.shape[0]
+    
+    def __getitem__(self, idx):
+        return self.Y[idx,:], self.X[idx,:]
+
+## Recognition network
+class RecognitionNet(nn.Module):
+    def __init__(self, C, G, h=6):
+        super(RecognitionNet, self).__init__()
+        self.hidden_1 = nn.Linear(G, h).double()
+        self.hidden_2 = nn.Linear(h, C+1).double()
+
+    def forward(self, x):
+        x = self.hidden_1(x)
+        x = F.relu(x)
+        x = self.hidden_2(x)
+        x = F.softmax(x, dim=1)
+        return x
+
+## Parameters and initialization
+
+log_sigma_init = np.log(Y_np.std(0))
+mu_init = np.log(Y_np.mean(0))
+
+## prior on z
+log_alpha = torch.log(torch.ones(C+1) / (C+1))
+
+recog = RecognitionNet(C, G)
+
+log_sigma = Variable(torch.from_numpy(log_sigma_init.copy()), requires_grad = True)
+mu = Variable(torch.from_numpy(mu_init.copy()), requires_grad = True)
+log_delta = Variable(0 * torch.ones((G,C+1)), requires_grad = True)
+delta = torch.exp(log_delta)
+rho = torch.from_numpy(marker_mat)
+
+## Construct optimizer
+learning_rate = 1e-2
+params = [log_sigma, mu, log_delta] + list(recog.parameters())
+optimizer = torch.optim.Adam(params, lr=args.learning_rate)
+
+## Declare pytorch forward fn
+def forward(log_delta, log_sigma, mu, log_alpha, rho, Y, X):
+    
+    Y_spread = Y.reshape(-1, G, 1).repeat(1, 1, C+1)
+    
+    mean = torch.exp(torch.exp(log_delta) * rho + mu.reshape(-1, 1))
+    dist = Normal(mean, torch.exp(log_sigma).reshape(-1, 1))
+    
+
+    log_p_y = dist.log_prob(Y_spread)
+
+    log_p_y_on_c = log_p_y.sum(1)
+    
+    gamma = recog.forward(X)
+
+    elbo = ( gamma * (log_p_y_on_c + log_alpha - torch.log(gamma)) ).sum()
+    
+    return -elbo
+
+
+## Make dataloader
+dset = IMCDataSet(Y_np)
+dataloader = DataLoader(dset, batch_size=min(args.batch_size,N), shuffle=True)
+
+
+## Run training loop
+epochs = args.epochs
+losses = np.empty(epochs)
+
+for ep in range(epochs):
+    L = None
+    
+    for batch in dataloader:
+        Y,X = batch
+        optimizer.zero_grad()
+        L = forward(log_delta, log_sigma, mu, log_alpha, rho, Y, X)
+        L.backward()
+        optimizer.step()
+    
+    l = L.detach().numpy()
+    losses[ep] = l
+    print(l)
+
+## Save output
+g = recog.forward(dset.X).detach().numpy()
+
+assignments = pd.DataFrame(g)
+assignments.columns = cell_types + ['Other']
+assignments.index = core_names
+
+assignments.to_csv(args.output_csv)
+
+print("Done!")
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="astir-pkg-JinelleH",
+    version="0.0.1",
+    author="Jinyu Hou",
+    author_email="jhou@lunenfeld.ca",
+    description=" ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/camlab-bioml/astir-pkg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+    python_requires='>=3.6',
+)
