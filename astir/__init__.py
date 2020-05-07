e2e47ebf5aff290f90b69512cd7ba90f97bd84f1
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2020-05-07 11:53:54 -0400

    simple package completed. unnecessary files removed

--- astir/__init__.py
+++ astir/__init__.py
@@ -1,13 +1,10 @@
 from .astir import Astir
 
-name = "Astir"
-
-print(__package__)
 
 __all__ = [
     "Astir" 
 ]
 
-ast = Astir("./BaselTMA_SP43_115_X4Y8.csv", "jackson-2020-markers.yml", 
-    "./BaselTMA_SP43_115_X4Y8_assignment.csv")
-ast.fit()
+# ast = Astir("./BaselTMA_SP43_115_X4Y8.csv", "jackson-2020-markers.yml", 
+#     "./BaselTMA_SP43_115_X4Y8_assignment.csv")
+# ast.fit()
