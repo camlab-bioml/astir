305507f53567ea8a4083b6268a65c4cd7f7c7549
Kieran Campbell <kieranrcampbell@gmail.com>
2020-05-20 12:49:36 -0400

    Introduce cell type specific covariance

--- astir/models/celltype.py
+++ astir/models/celltype.py
@@ -10,7 +10,7 @@ import warnings
 
 import torch
 from torch.autograd import Variable
-from torch.distributions import Normal, StudentT
+from torch.distributions import Normal, StudentT, MultivariateNormal
 import torch.nn.functional as F
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
@@ -48,36 +48,36 @@ class CellTypeModel:
         """Initialize parameters and design matrices.
         """
 
-
-        self.initializations = {
-            "mu": 0.5 * torch.from_numpy(np.log(self.Y_np.mean(0).copy().reshape((-1,1)))),
-            "log_sigma": torch.from_numpy(np.log(self.Y_np.std(0)).copy())
+        # Establish data
+        self.data = {
+            "log_alpha": torch.log(torch.ones(self.C+1) / (self.C+1)),
+            "rho": torch.from_numpy(self.marker_mat)
         }
 
+        # Initialize mu, log_delta
+        t = torch.distributions.Normal(torch.tensor(0.), torch.tensor(0.2))
+        log_delta_init = t.sample((self.G,self.C+1))
+
+        mu_init = torch.from_numpy(np.log(self.Y_np.mean(0).copy()))
+        mu_init = mu_init - (self.data['rho'] * torch.exp(log_delta_init)).mean(1)
+        mu_init = mu_init.reshape(-1,1)
+
+        # Create initialization dictionary
+        self.initializations = {
+            "mu":  mu_init,
+            "log_sigma": torch.from_numpy(np.log(self.Y_np.std(0)).copy()),
+            "log_delta": log_delta_init,
+            "p": torch.zeros(self.G, self.C+1)
+        }
 
         # Add additional columns of mu for anything in the design matrix
         P = self.dset.design.shape[1]
         self.initializations['mu'] = torch.cat( \
             [self.initializations['mu'], torch.zeros((self.G, P-1)).double()],
             1)
-
-        t = torch.distributions.Normal(torch.tensor(0.), torch.tensor(0.2))
-        log_delta_init = t.sample((self.G,self.C+1))
-
-        print(f"log_delta_init mean: {torch.mean(log_delta_init)}")
-
-        ## prior on z
-        self.variables = {
-            "log_sigma": Variable(self.initializations['log_sigma'], requires_grad = True),
-            "mu": Variable(self.initializations["mu"], requires_grad = True),
-            "log_delta": Variable(log_delta_init, requires_grad = True)
-        }
-
-        self.data = {
-            "log_alpha": torch.log(torch.ones(self.C+1) / (self.C+1)),
-            "delta": torch.exp(self.variables["log_delta"]),
-            "rho": torch.from_numpy(self.marker_mat)
-        }
+        
+        # Create trainable variables
+        self.variables = {n: Variable(v, requires_grad=True) for (n,v) in self.initializations.items()}
 
         if self.include_beta:
             self.variables['beta'] = Variable(\
@@ -110,11 +110,19 @@ class CellTypeModel:
                 min_delta = torch.min(delta_tilde, 1).values.reshape((self.G,1))
             mean = mean + min_delta * torch.tanh(self.variables["beta"]) * (1 - self.data["rho"]) 
 
-        dist = Normal(torch.exp(mean), torch.exp(self.variables["log_sigma"]).reshape(-1, 1))
+        # now do the variance modelling
+        p = torch.sigmoid(self.variables["p"])
+        corr_mat = torch.einsum('gc,hc->cgh', self.data['rho'] * p, self.data['rho'] * p) * \
+            (1 - torch.eye(self.G)) + torch.eye(self.G)
+        self.cov_mat = torch.einsum('i,j->ij', torch.exp(self.variables["log_sigma"]), torch.exp(self.variables["log_sigma"]))
+        self.cov_mat = self.cov_mat * corr_mat
+
+        dist = MultivariateNormal(loc=torch.exp(mean).permute(0,2,1), covariance_matrix=self.cov_mat)
+        # dist = Normal(torch.exp(mean), torch.exp(self.variables["log_sigma"]).reshape(-1, 1))
         # dist = StudentT(torch.tensor(1.), torch.exp(mean), torch.exp(self.variables["log_sigma"]).reshape(-1, 1))
 
-        log_p_y = dist.log_prob(Y_spread)
-        log_p_y_on_c = log_p_y.sum(1)
+        log_p_y_on_c = dist.log_prob(Y_spread.permute(0,2,1))
+        # log_p_y_on_c = log_p_y.sum(1)
         gamma = self.recog.forward(X)
         elbo = ( gamma * (log_p_y_on_c + self.data["log_alpha"] - torch.log(gamma)) ).sum()
         
@@ -123,7 +131,7 @@ class CellTypeModel:
     ## Todo: an output function
     def __init__(self, Y_np: np.array, type_dict: Dict,  \
                 N: int, G: int, C: int, type_mat: np.array, \
-                include_beta = True, design = None
+                include_beta = False, design = None
                 ) -> None:
         """Initializes an Astir object
 
@@ -142,6 +150,8 @@ class CellTypeModel:
         :raises NotClassifiableError: raised when randon seed is not an integer
         """
         self.losses = None # losses after optimization
+
+        self.cov_mat = None # temporary -- remove
 
         self.type_dict = type_dict
 
