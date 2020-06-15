f75b47c42fa484eba31bbe00078331449d6c10ca
sunyunlee <sunyun.lee@mail.utoronto.ca>
2020-06-15 18:57:59 -0400

    setup.py update packages

--- setup.py
+++ setup.py
@@ -31,7 +31,8 @@ setuptools.setup(
         "matplotlib",
         "loompy",
         "tqdm",
-        "anndata"
+        "anndata",
+        "rootpath"
     ],
     include_package_data=True,
     zip_safe=False,
