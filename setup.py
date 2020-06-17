9d1c3664c28b3ebc67cc8637bb11b2dafbce70c9
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2020-06-17 17:03:45 -0400

    minor change

--- setup.py
+++ setup.py
@@ -38,12 +38,8 @@ setuptools.setup(
         "seaborn",
         "jupyter_client",
         "ipykernel",
-<<<<<<< HEAD
-        "ipython" "h5py",
-=======
         "ipython",
         "h5py"
->>>>>>> 785704e78d58b0a880f4e0f98922083a2f4915f5
     ],
     include_package_data=True,
     zip_safe=False,
