e70232337a5c9d61b93b8ab7b776971b86fcb662
sunyunlee <sunyun.lee@mail.utoronto.ca>
2020-06-22 10:57:57 -0400

    script file ..

--- astir/tests/astir_bash.py
+++ astir/tests/astir_bash.py
@@ -6,17 +6,17 @@ import yaml
 
 import os
 import sys
-module_path = os.path.abspath(os.path.join('.'))
-print("####", module_path)
+# module_path = os.path.abspath(os.path.join('.'))
+# print("####", module_path)
+# if module_path not in sys.path:
+#     sys.path.append(module_path)
+# print(sys.path)
+
+module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
 print(sys.path)
 
-# module_path = os.path.abspath(os.path.join('..'))
-# if module_path not in sys.path:
-#     sys.path.append(module_path)
-# print(sys.path)
-#
 # module_path = os.path.abspath(os.path.join('../..'))
 # print(module_path)
 # if module_path not in sys.path:
