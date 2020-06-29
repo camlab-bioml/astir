d9b95f011d57fd6575e143a08c6ab4d7be686932
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2020-06-29 11:24:26 -0400

    minor change

--- astir/models/celltype_recognet.py
+++ astir/models/celltype_recognet.py
@@ -8,7 +8,7 @@ class TypeRecognitionNet(nn.Module):
     """ Type Recognition Neural Network.
 
     :param C: number of classes
-    :param G: number of proteins
+    :param G: number of features
     :param hidden_size: size of hidden layers
     """
     def __init__(self, C: int, G: int, hidden_size=10) -> None:
