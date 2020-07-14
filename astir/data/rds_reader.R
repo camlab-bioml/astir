6d64afcf1468b74d71f66c6a6d092e750d0baaa9
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2020-07-14 18:06:42 -0400

    tutorial for rds convert and model loading

--- astir/data/rds_reader.R
+++ astir/data/rds_reader.R
@@ -3,6 +3,7 @@ suppressPackageStartupMessages({
   library(argparse)
   library(taproom)
 })
+options(warn=-1)
 
 parser <- ArgumentParser(description='Input .rds file to be processed')
 parser$add_argument('rds_in', type='character', help='input rds file')
@@ -10,26 +11,28 @@ parser$add_argument('csv_out', type='character', help='output csv file')
 parser$add_argument('--assay', type='character', help='assay')
 parser$add_argument('--design_col', type='character', help='design column')
 parser$add_argument('--design_csv', type='character', help='output design csv file')
-parser$add_argument('--winsorize', type='double', help="percentage remaining after winsorizing")
+parser$add_argument('--winsorize', type='character', help="percentage remaining after winsorizing")
 args <- parser$parse_args()
 
 sce <- readRDS(args$rds_in)
 
-win = args$winsorize
-winsorize(sce, exprs_values=args$assay, w_limits=c(win/2, 1-win/2))
+win = as.numeric(args$winsorize)
+for (channel in rownames(sce)) {
+  sce[channel, ] = winsorize(sce[channel, ], exprs_values=args$assay, w_limits=c((1-win)/2, (1+win)/2))
+}
 
 # print(sce)
 sce_df <- t(data.frame(assay(sce, args$assay)))
 write.csv(sce_df, args$csv_out, row.names=TRUE)
 
 design = args$design_col
-if (!is.null(design)) {
+if (design != "") {
   mm <- as.data.frame(colData(sce))
   ind = which(colnames(mm) == design)
   colnames(mm)[ind] <- 'target_col'
   # print(head(mm))
   design_mm <- model.matrix(~target_col, mm)
-  if (!is.null(args$design_csv)) {
+  if (args$design_csv != "") {
     write.csv(design_mm, args$design_csv, row.names=TRUE)
   }
 }
