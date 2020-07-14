c0c583aaa7c43a90b00342ef192c67585f7901d1
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2020-07-14 12:41:18 -0400

    winsorization added to converter

--- astir/data/rds_reader.R
+++ astir/data/rds_reader.R
@@ -1,6 +1,7 @@
 suppressPackageStartupMessages({
   library(SingleCellExperiment)
   library(argparse)
+  library(taproom)
 })
 
 parser <- ArgumentParser(description='Input .rds file to be processed')
@@ -9,9 +10,14 @@ parser$add_argument('csv_out', type='character', help='output csv file')
 parser$add_argument('--assay', type='character', help='assay')
 parser$add_argument('--design_col', type='character', help='design column')
 parser$add_argument('--design_csv', type='character', help='output design csv file')
+parser$add_argument('--winsorize', type='double', help="percentage remaining after winsorizing")
 args <- parser$parse_args()
 
 sce <- readRDS(args$rds_in)
+
+win = args$winsorize
+winsorize(sce, exprs_values=args$assay, w_limits=c(win/2, 1-win/2))
+
 # print(sce)
 sce_df <- t(data.frame(assay(sce, args$assay)))
 write.csv(sce_df, args$csv_out, row.names=TRUE)
@@ -23,5 +29,7 @@ if (!is.null(design)) {
   colnames(mm)[ind] <- 'target_col'
   # print(head(mm))
   design_mm <- model.matrix(~target_col, mm)
-  write.csv(design_mm, args$design_csv, row.names=TRUE)
+  if (!is.null(args$design_csv)) {
+    write.csv(design_mm, args$design_csv, row.names=TRUE)
+  }
 }
