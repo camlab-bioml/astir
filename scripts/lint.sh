a2edf7bd3238d68f5ed4c3254f08d8ff27fc931a
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2020-08-25 17:00:26 +0800

    disabled mypy autocheck

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-mypy astir --disallow-untyped-defs
+# mypy astir --disallow-untyped-defs
 black astir tests --check
 # isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty astir astir tests
