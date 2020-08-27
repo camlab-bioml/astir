13d8ef159046321905cf71a8d7a445c429862bd4
sunyunlee <sunyun.lee@mail.utoronto.ca>
2020-08-27 18:52:57 -0400

    lint astir.py

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,5 +5,7 @@ set -x
 
 # mypy astir --disallow-untyped-defs
 # black astir tests --check
-black tests
+black tests --check
+black astir/astir.py
+black astir tests --check
 # isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty astir astir tests
